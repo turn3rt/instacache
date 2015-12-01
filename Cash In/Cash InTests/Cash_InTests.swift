3c18573a689237e0bf8ba4aad79d72c43b51705a
turn3rt <turner.thornberry@setmine.com>
2015-12-01 15:15:27 -0500

    LETS DO THIS

---
+++ Cash In/Cash InTests/Cash_InTests.swift
@@ -0,0 +1,36 @@
+//
+//  Cash_InTests.swift
+//  Cash InTests
+//
+//  Created by Turner Thornberry on 12/1/15.
+//  Copyright © 2015 MChackathon. All rights reserved.
+//
+
+import XCTest
+@testable import Cash_In
+
+class Cash_InTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
